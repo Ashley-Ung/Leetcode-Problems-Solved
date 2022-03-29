054b6e6217c41943ae2ae33f08f5beed3d6b53bc
Ashley Ung <62251927+ung8005@users.noreply.github.com>
2022-03-28 21:18:50 -0700

    removeNthNode [Medium]

---
+++ removeNthNode.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd (ListNode* head, int n) {
+        int counter = 0;
+        int a; 
+        ListNode* HeadPtr = head;
+        while (HeadPtr){
+            counter++;
+            HeadPtr = HeadPtr->next;
+        }
+        a = counter-n-1;
+        if (a<0) {
+            head = head->next;
+            return head;
+        }
+        HeadPtr = head;
+        while (a--){
+  
+            HeadPtr = HeadPtr->next;
+        }
+        if ((HeadPtr->next)->next) HeadPtr->next = (HeadPtr->next)->next;
+        else HeadPtr->next = NULL;
+        
+        return head;
+    }
+};
